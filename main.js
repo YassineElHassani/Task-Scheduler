0f15f4bd562b691a4a6eec5b568f1ccee3977493
Yassine El hassani <yassine.ds.404@gmail.com>
2024-11-01 11:36:51 +0100

    fixing the sizing of the table frame

--- main.js
+++ main.js
@@ -5,14 +5,24 @@ let date = document.getElementById("date");
 
 function addTasks() {
     if(name.value && desc.value  && type.value && date.value != "") {
-         let tableBody = document.getElementById("task-list");
+        let tableBody = document.getElementById("task-list");
+        
+        let color = ""
+        if(type.value == "To Do") {
+            color = "#0369a1";
+        } else if(type.value == "Doing") {
+            color = "#ca8a04";
+        } else {
+            color = "#22c55e";
+        }
+
         tableBody.innerHTML += `
         <tr class="border-solid">
-            <td>${name.value}</td>
-            <td>${desc.value}</td>
-            <td>${type.value}</td>
-            <td>${date.value}</td>
-            <td class="flex flex items-center gap-[10px] border-solid">
+            <td class="px-6 py-3">${name.value}</td>
+            <td class="px-6 py-3">${desc.value}</td>
+            <td class="px-6 py-3 text-[${color}] rounded-b-[5px] rounded-t-[5px]">${type.value}</td>
+            <td class="px-6 py-3">${date.value}</td>
+            <td class="flex flex items-center gap-[10px] border-solid px-6 py-3">
                 <button onclick="updateTasks(event)" class="flex items-center justify-center w-[30px] h-[30px]"><img src="./Assest/images/edit.png" class="w-[20px] h-[20px]"></button>
                 <button onclick="deleteTask(event)" class="flex items-center justify-center w-[30px] h-[30px]"><img src="./Assest/images/trash.png" class="w-[20px] h-[20px]"></button>
             </td>
@@ -27,8 +37,8 @@ function addTasks() {
 
 let tr;
 function updateTasks(event) {
-     tr = event.currentTarget.parentElement.parentElement;
-    console.log(event);
+    tr = event.currentTarget.parentElement.parentElement;
+    
     name.value = tr.getElementsByTagName("td")[0].textContent;
     desc.value = tr.getElementsByTagName("td")[1].textContent;
     type.value = tr.getElementsByTagName("td")[2].textContent;
